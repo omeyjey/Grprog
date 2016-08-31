38f652588826efce64154e8e5124bed4df9632f2
Morten Omholt-Jensen <Morten@mortens-macbook-pro.local>
2016-08-31 17:49:07 +0200

    Initial Commit

---
+++ grprog/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grprog
+//
+//  Created by Morten Omholt-Jensen on 31/08/16.
+//  Copyright © 2016 Morten Omholt-Jensen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
