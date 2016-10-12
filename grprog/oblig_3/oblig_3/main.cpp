bd024889725eeb1cb5f9b3822aee2df3bd1d13b5
omeyjey <morten@omholt-jensen.com>
2016-10-12 17:07:50 +0200

    Add files via upload

---
+++ grprog/oblig_3/oblig_3/main.cpp
@@ -0,0 +1,196 @@
+// Morten Omholt-Jensen, obligg 3.
+
+// Programmet skal gjøre det mulig for brukren å skrive inn tre forskjellige
+// brøker og videre, velge en av fire opperasjoner på brøkene valgt.
+// Programmet skal da regne ut mattestykket til brukren og skrive det ut.
+// Når det er ferdig kjører det om igjen fra der brukeren kan velge brøker fram
+// til brukeren skriver inn 'q' og avslutter programmet.
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct fractions                            // Struct om en brøk
+{
+    int numerator;                          // Innholder teller og nevner
+    int denomerator;
+};
+
+int main()
+{
+    fractions fraction1,                    // Lager nye structer med variabler
+    fraction2,
+    fraction3,
+    temp,                                   // Lager variabler temp1, 2, 3
+    temp1,
+    temp2;
+
+    char svar;                              // Starte om igjen eller 'q' for
+    char opperasjon;                        // avslutte og hvilken opperasjon
+                                            // brukeren vil bruke.
+    
+    do                                      // Løkker som ber brukeren skrive
+    {                                       // inn teller og nevner for 3 brøker
+        cout << "Brøk nr 1 (teller nevner): ";
+        cin >> fraction1.numerator >> fraction1.denomerator;
+    
+        cout << "Brøk nr 2 (teller nevner): ";
+        cin >> fraction2.numerator >> fraction2.denomerator;
+    
+        cout << "Brøk nr 3 (teller nevner): ";
+        cin >> fraction3.numerator >> fraction3.denomerator;
+        
+    } while (fraction3.denomerator == 0 || fraction2.denomerator == 0 || fraction1.denomerator == 0);
+       
+    do                                      // Løkke som kjører resten av
+    {                                       // Programmet
+        do                                  // Løkke som ber brukeren velge brøk
+        {
+            cout << "Hvilke brøker vil du utføre opperasjoner på?" << endl;
+            cout << "a: Brøk nr 1 og 2" << endl;
+            cout << "b: Brøk nr 1 og 3" << endl;
+            cout << "c: Brøk nr 2 og 3" << endl;
+            
+            cout << "Skriv inn a, b, eller c utifra valgene over "
+                 << "eller q for å avslutte: ";
+            cin >> svar;                    // Brukerens svar
+            
+        } while (svar != 'a' && svar != 'b' && svar != 'c' && svar != 'q');
+        
+        while (svar != 'q')
+        {
+            if (svar == 'a')                // Gjør om temp1 og 2 til 
+            {                               // brukerens valgte brøker
+                temp1 = fraction1;
+                
+                temp2 = fraction2;
+                temp2 = fraction3;
+                
+            } else if (svar == 'b')
+            {
+                temp1 = fraction1;
+                temp1 = fraction1;
+                
+                temp2 = fraction3;
+                temp2 = fraction3;
+                
+            } else if (svar == 'c')
+            {
+                temp1 = fraction2;
+                temp1 = fraction2;
+                
+                temp2 = fraction3;
+                temp2 = fraction3;
+            } 
+
+        do                                      // Henter inn brukrens valg av
+        {                                       // opperasjon på brøkene.
+            cout << "Velg en opperasjon på"
+                 << "brøkene du har valgt (+ - * /): ";
+            cin >> opperasjon;
+            
+        } while(opperasjon != '+' && opperasjon != '-' && 
+                opperasjon != '*' && opperasjon != '/');
+        
+        if (opperasjon == '+')   // Regner ut og printer ut
+        {                                       // brukerens brøker og svar
+            temp.numerator = (temp1.numerator * // Setter temp lik brukerens
+                            temp2.denomerator) +// svar.
+                            (temp1.denomerator *
+                             temp2.numerator);
+            temp.denomerator = (temp1.denomerator *
+                                temp2.denomerator);
+            
+            cout << setw(3) << temp1.numerator;
+            cout << setw(9) << temp2.numerator << endl;
+            cout << " ---  +  ----" << endl;
+            cout << setw(3) << temp1.denomerator << "        ";
+            cout << temp2.denomerator << endl;
+            cout << endl;
+            cout << "=" << setw(4) << temp.numerator << endl;
+            cout << setw(6) << "----" << endl;
+            cout << setw(6) << temp.denomerator << endl;
+            
+            cout << endl;
+            cout << endl;
+            
+        } else if (opperasjon == '-') // Gjør det samme for alle
+        {                                            // opperasjonene.
+            temp.numerator = (temp1.numerator * temp2.denomerator) -
+            (temp1.denomerator * temp2.numerator);
+            
+            temp.denomerator = (temp1.denomerator * temp2.denomerator);
+            
+            cout << setw(3) << temp1.numerator;
+            cout << setw(9) << temp2.numerator << endl;
+            cout << " ---  -  ----" << endl;
+            cout << setw(3) << temp1.denomerator << "        ";
+            cout << temp2.denomerator << endl;
+            cout << endl;
+            cout << "=" << setw(4) << temp.numerator << endl;
+            cout << setw(6) << "----" << endl;
+            cout << setw(6) << temp.denomerator << endl;
+            
+            cout << endl;
+            cout << endl;
+            
+        } else if (opperasjon == '*')    // Multplikasjon
+        {
+            temp.numerator = (temp1.numerator * temp2.numerator);
+            temp.denomerator = (temp1.denomerator * temp2.denomerator);
+            
+            cout << setw(3) << temp1.numerator;
+            cout << setw(9) << temp2.numerator << endl;
+            cout << " ---  *  ----" << endl;
+            cout << setw(3) << temp1.denomerator << "        ";
+            cout << temp2.denomerator << endl;
+            cout << endl;
+            cout << "=" << setw(4) << temp.numerator << endl;
+            cout << setw(6) << "----" << endl; 
+            cout << setw(6) << temp.denomerator << endl; 
+            
+            cout << endl;
+            cout << endl;
+            
+        } else if (opperasjon == '/')    // Divisjon
+        {
+            temp.numerator = (temp1.numerator * temp2.denomerator);
+            temp.denomerator = (temp1.denomerator * temp2.numerator);
+            
+            cout << setw(4) << temp1.numerator;
+            cout << setw(8) << temp2.numerator << endl;
+            cout << " ---  /  ----" << endl; 
+            cout << setw(3) << temp1.denomerator << "        ";
+            cout << temp2.denomerator << endl;
+            cout << endl;
+            cout << "=" << setw(4) << temp.numerator << endl;
+            cout << setw(6) << "----" << endl; 
+            cout << setw(5) << temp.denomerator << endl; 
+            
+            cout << endl;
+            cout << endl;
+            
+        }
+    }
+    
+    
+    } while (svar != 'q');                      // Løkken kjører hvis brukeren
+                                                // ikke har skrevet inn 'q'
+                                                // Avslutter programmet med en
+                                                // hyggelig melding.
+
+
+
+
+    cout << endl;                               
+    cout << endl;                               
+    cout << "*****************" << endl;
+    cout << "Avslutter program" << endl;
+    cout << "  Ha en fin dag!"  << endl;	
+    cout << "*****************" << endl;
+    
+    
+    return 0;
+}
